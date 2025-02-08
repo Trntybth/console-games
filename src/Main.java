3f36178a232fd06e245d88c251e1fcaf618ce33c
Trinity Booth <trinitybooth89@gmail.com>
2025-02-08 17:59:03 +0000

    feat: added the 'computer choice' String array with random position selector, bound of 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -21,6 +22,13 @@ public class Main {
             if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")){
                 System.out.println("You entered an invalid choice; try again with either 'rock' or 'paper' or 'scissors'.");
             }
+
+            // Computer choice String array & random array position selector with bound of 3
+            String[] choices = {"rock", "paper", "scissors"}; // String array for switch statement
+            Random random = new Random();
+            String randomComputerChoice = choices[random.nextInt(3)]; // Random
+            System.out.println("I have chosen " + randomComputerChoice + ".");
+
 
         }
 
