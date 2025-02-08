56ff6c8d692ff94aa16accb64d2df47b704e3a10
Trinity Booth <trinitybooth89@gmail.com>
2025-02-08 17:23:58 +0000

    feat: add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("You are about to enter a very serious game of rock paper scissors.");
+
+        // Create scanner for user input detection
+        Scanner scanner = new Scanner(System.in);
+
+
+
     }
+
 }
